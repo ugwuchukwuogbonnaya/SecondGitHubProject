fbbc7b36f2714069114322ba6756dd8e76ef5578
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-08-08 11:26:42 +0100

    Initial Commit

---
+++ src/main/java/com/casmir/org/SecondGitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.casmir.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondGitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondGitHubProjectApplication.class, args);
+	}
+
+}
