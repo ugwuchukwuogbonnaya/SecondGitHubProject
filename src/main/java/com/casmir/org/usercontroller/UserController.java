055af2a85db70c299d8b52319c5f81d513e42aa3
ugwuchukwuogbonnaya <stcasmir4excel@gmail.com>
2022-08-08 11:58:03 +0100

    user home rest api completed

--- src/main/java/com/casmir/org/usercontroller/UserController.java
+++ src/main/java/com/casmir/org/usercontroller/UserController.java
@@ -1,5 +1,17 @@
 package com.casmir.org.usercontroller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+	
+	@GetMapping("/home")
+	public String home() {
+		
+		return "Welcome to USER HOME PAGE";
+	}
 
 }
